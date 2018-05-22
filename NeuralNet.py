ee808c2162a6e79a96384303c26d5a0de471c9c4
meperson <jasontiemann@MacBook-Pro-2.local>
2018-05-21 22:25:17 -0500

    Finalised NeuralNet Lib

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,9 +1,9 @@
 import random
 
 class node:
-	value = 0
-	weight = 0
+	
 	def __init__(self):
+		self.value = 0
 		self.weight=random.uniform(-2,2)
 	def clear(self):
 		self.value=0
@@ -18,91 +18,102 @@ class node:
 			self.weight+=random.uniform(-.5,.5)
 
 class nodeRow:
-	row=[]
 	def __init__(self,length):
+		self.row=[]
 		for i in range(length):
 			self.row.append(node())
 	def add(self,inputRow):
-		for i in range(len(inputRow)-1):
+		for i in range(len(self.row)):
 			self.row[i].add(inputRow[i])
 	def clear(self):
 		for i in range(len(self.row)):
 			self.row[i].clear()
 	def activate(self):
 		outputRow=[]
-		for i in range(len(self.row)-1):
-			outputRow.append(self.row[i].activate)
+		for i in range(len(self.row)):
+			outputRow.append(self.row[i].activate())
+		return outputRow
 	def mutate(self):
-		for i in range(len(self.row)-1):
+		for i in range(len(self.row)):
 			self.row[i].mutate()
+	def setWeight(self,rowWeight):
+		self.row = rowWeight
+	def getAllWeight(self):
+		return self.row
 	def getWeight(self,place):
 		return self.row[place]
 	def getLen(self):
 		return len(self.row)
 	def combine(self,secondNodeRow):
 		newRow=[]
-		for i in range(secondNodeRow.getLen()-1):
+		for i in range(secondNodeRow.getLen()):
 			if(random.random()>.5):
 				newRow.append(self.row[i])
 			else:
 				newRow.append(secondNodeRow.getWeight(i))
-		return newRow
+		outRow = nodeRow(0)
+		outRow.setWeight(newRow)
+		return outRow
 
 class connectionRow:
-	weightList = []
 	def __init__(self,length):
+		self.weightList = []
 		for i in range(length):
 			self.weightList.append(random.uniform(-2,2));
 	def getWeight(self, position):
 		return self.weightList[position]
+	def setWeight(self, newWeightList):
+		self.weightList=newWeightList
 	def mutate(self):
-		for i in range(len(self.weightList)-1):
+		for i in range(len(self.weightList)):
 			if random.random()>.5:
 				self.weightList[i]+=random.uniform(-.5,.5)
 	def getLen(self):
 		return len(self.weightList)
 	def combine(self,secondConnection):
 		newWeightList=[]
-		for i in range(len(self.weightList)-1):
+		for i in range(len(self.weightList)):
 			if random.random()>.5:
 				newWeightList.append(self.weightList[i])
 			else:
 				newWeightList.append(secondConnection.getWeight(i))
-		return newWeightList
+		newConnection = connectionRow(0)
+		newConnection.setWeight(newWeightList)
+		return newConnection
 
 
 class net:
-	connectors = []
-	nodeRows = []
 	def __init__(self,layerWidths):
-		for i in range(len(layerWidths)-1):
+		self.connectors = []
+		self.nodeRows = []
+		for i in range(len(layerWidths)):
 			self.nodeRows.append(nodeRow(layerWidths[i]))
-			if i < len(layerWidths)-2:
+			if i < len(layerWidths)-1:
 				tempLayer = []
 				for fromNode in range(layerWidths[i]):
 					tempLayer.append(connectionRow(layerWidths[i+1]))
 				self.connectors.append(tempLayer)
-		print(self.nodeRows)
-		print("\n\n\n")
 
 	def activate(self, inputRow):
 		self.nodeRows[0].add(inputRow)
 		currentInput=self.nodeRows[0].activate()
 		self.nodeRows[0].clear()
-		for nodeLayer in range(len(self.nodeRows)-2):
-			tempOutput=[0]*len(self.nodeRows[nodeLayer+1])
-			for nodeOutput in currentInput:
-				for connectNum in self.connectors[nodeLayer].getLen():
-					tempOutput[connectNum]+=nodeOutput*self.connectors.getWeight(connectNum)
-			currentInput=self.nodeLayer.activate(tempOutput)
-			self.nodeLayer.clear()
+		for nodeLayer in range(len(self.nodeRows)-1):
+			for node in range(len(currentInput)):
+				tempOutput=[]
+				for connectNum in range(self.connectors[nodeLayer][0].getLen()):
+					tempOutput.append(currentInput[node]*self.connectors[nodeLayer][node].getWeight(connectNum))
+				self.nodeRows[nodeLayer+1].add(tempOutput)
+			currentInput=self.nodeRows[nodeLayer].activate()
+			self.nodeRows[node].clear()
 		return currentInput
 
 	def mutate(self):
-		for i in range(len(self.nodeRows)-1):
+		for i in range(len(self.nodeRows)):
 			self.nodeRows[i].mutate()
-		for i in range(len(self.connectors)-1):
-			self.connectors[i].mutate()
+		for fromConnect in range(len(self.connectors)):
+			for toConnect in range(len(self.connectors[fromConnect])):
+				self.connectors[fromConnect][toConnect].mutate()
 	def setConnectors(self,connectionList):
 		self.connectors = connectionList
 	def setNodeRows(self,nodeRows):
@@ -114,15 +125,13 @@ class net:
 	def haveChild(self,secondNet):
 		newConnectors=[]
 		newNodeRows=[]
-		for nodeRow in range(len(self.nodeRows)-1):
+		for nodeRow in range(len(self.nodeRows)):
 			newNodeRows.append(self.nodeRows[nodeRow].combine(secondNet.getNodeRows(nodeRow)))
-			# print(self.nodeRows[nodeRow].combine(secondNet.getNodeRows(nodeRow)))
-			if nodeRow < len(self.nodeRows)-2:
-				for connectNum in range(len(self.connectors[nodeRow])-1):
-					newConnectors.append(self.connectors[nodeRow][connectNum].combine(secondNet.getConnectors(nodeRow)[connectNum]))
-		print("Child Node Rows")
-		print(newNodeRows)
-		print('\n\n\n')
+			if nodeRow < len(self.nodeRows)-1:
+				tempConnectionRow=[]
+				for connectNum in range(len(self.connectors[nodeRow])):
+					tempConnectionRow.append(self.connectors[nodeRow][connectNum].combine(secondNet.getConnectors(nodeRow)[connectNum]))
+			newConnectors.append(tempConnectionRow)
 		newNet = net([])
 		newNet.setConnectors(newConnectors)
 		newNet.setNodeRows(newNodeRows)
